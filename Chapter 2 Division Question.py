161c006550272cfc12a77546206ce1e73e485e8d
Eli-Bilyeu <bilyeue2@student.swosu.edu>
2024-12-09 14:35:13 -0600

    Create Chapter 2 Division Question.py

---
+++ Chapter 2 Division Question.py
@@ -0,0 +1,7 @@
+user_num = int(input("What is the number you want to divide with: "))
+div_num = int(input("What is the number you want to divie by: "))
+
+result1 = (user_num // div_num)
+result2 = (result1 // div_num)
+
+print(user_num, result1, result2)
